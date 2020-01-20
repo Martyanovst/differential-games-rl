aa01682b2bdc3ab5b743489474ac2ff31db714ea
Martyanovst <martyanovst@gmail.com>
2020-01-21 01:57:50 +0500

    doing naf agent

--- DQN.py
+++ DQN.py
@@ -5,52 +5,62 @@ import random
 import gym
 import matplotlib.pyplot as plt
 
-
 class Network(nn.Module):
+    def __init(self, layers, hidden_activation, output_activation):
+        super().__init__()
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.layers_count = len(layers)
+        self.output_layer = nn.Linear(layers[self.layers_count - 2], self.layers_count - 1])
+        self.init_hidden_layers_(layers)
+
+    def init_hidden_layers_(self, layers):
+        self.hidden_layers = []
+        for i in range(1, len(layers) - 1):
+            previous_layer = layers[i - 1]
+            current_layear = layers[i]
+            linear = nn.Linear(previous_layer, current_layear)
+            self.hidden_layers.append(linear)
+        
+
+    def forward(self, tensor):
+        hidden = tensor
+        for layer in self.hidden_layers:
+            hidden = self.hidden_activation(layer(hidden))
+        output = self.output_activation(self.output_layer(hidden))
+        return hidden
+
+
+class NAF_Network(nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.linear_1 = nn.Linear(input_dim, 64)
-        self.linear_2 = nn.Linear(64, 64)
+        self.mu = Network([input_dim, 64, 64, 32, output_dim], nn.Relu(), nn.Tahn())
+        self.P = Network([input_dim, 64, 64, 32, int(output_dim * (output_dim + 1) / 2)], nn.Relu(), nn.Relu())
+        self.v = Network([input_dim, 32, 16, 1], nn.Relu(), nn.Tahn())        
 
-        self.mu_input = nn.Linear(64, 32)
-        self.mu_output = nn.Linear(32, output_dim)
+    def forward(self, tensor):
+        mu = self.mu(tensor)
+        L = self.P(tensor)
+        v = self.v(tensor)
+        return mu, L, v
 
-        self.P_input = nn.Linear(64, 32)
-        self.P_output = nn.Linear(32,int(output_dim * (output_dim + 1) / 2))
-        
-        self.V_input = nn.Linear(64, 32)
-        self.V_output = nn.Linear(32, 1)
-        
-        self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
+    def Q_value(self, tensor, action):
+        mu, L, v = self.forward(tensor)
+        P = L @ L.T
+        A = -1/2 * (action - mu).T @ P @ (action - mu)
+        return A + v
+    
+    def maximum_q_value(self, tensor):
+        return self.v(tensor)
 
-    def forward(self, input):
-        hidden = self.linear_1(input)
-        hidden = self.relu(hidden)
-        hidden = self.linear_2(hidden)
-        middleware = self.relu(hidden)
-
-        hidden_mu = self.mu_input(hidden)
-        hidden_P = self.P_input(hidden)
-        hidden_V = self.V_input(hidden)
-
-        hidden_mu = self.relu(hidden_mu)
-        hidden_P = self.relu(hidden_P)
-        hidden_V = self.relu(hidden_V)
-
-        mu = self.tanh(self.mu_output(hidden_mu))
-        P = self.P_output(hidden_P)
-        V = self.V_output(hidden_V)
-
-
-        return (mu, P, V)
+    def argmax_action(self, tensor):
+        return self.mu(tensor)
     
     def Q_value(self, state, action):
         mu, P, V = self.forward(state)
         A = - 1/2 * (action - mu).T * P * (action - mu) 
         return A + V
-
 
 
 class DQNAgent(nn.Module):
@@ -94,7 +104,6 @@ class DQNAgent(nn.Module):
         self.memory.append([state, action, reward, done, next_state])
         if len(self.memory) > self.memory_size:
             self.memory.pop(0)
-
         if len(self.memory) > self.batch_size:
             for _ in range(5):
                 batch = random.sample(self.memory, self.batch_size)
@@ -102,9 +111,12 @@ class DQNAgent(nn.Module):
                 states, actions, rewards, dones, next_states = list(
                     zip(*batch))
                 states = torch.FloatTensor(states)
-                q_values = self.q_normalized.Q_value(states)
+                actions = torch.FloatTensor(actions)
+                tensor = zip(states, actions)
+                
+                q_values = list(map(lambda inpt: self.q_normalized.Q_value(inpt[0], inpt[1]), tensor))
                 next_states = torch.FloatTensor(next_states)
-                next_q_values = self.q_target.Q_value(next_states)
+                next_q_values = self.q_target(next_states)
                 targets = q_values.clone()
                 for i in range(self.batch_size):
                     targets[i][actions[i]] = rewards[i] + self.gamma * \
