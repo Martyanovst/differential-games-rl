bf84a051c0f785c7a7ee65974c63b0037958d071
smartyanov <smartyanov@naumen.ru>
2021-04-19 18:47:20 +0500

    Dubins car + terminal pendulum

---
+++ models/naf_r_g.py
@@ -0,0 +1,106 @@
+import random
+from collections import deque
+from copy import deepcopy
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Q_model(nn.Module):
+    def __init__(self, v_model, action_shape, dt, r=1, g=1, action_max=1):
+        super().__init__()
+        self.v = v_model
+        self.dt = dt
+        self.r = r
+        self.g = g
+        self.action_max = action_max
+        self.action_shape = action_shape
+        self.tril_mask = torch.tril(torch.ones(
+            action_shape, action_shape), diagonal=-1).unsqueeze(0)
+        self.diag_mask = torch.diag(torch.diag(
+            torch.ones(action_shape, action_shape))).unsqueeze(0)
+
+    def mu(self, state):
+        v = self.v(state)
+        v.backward(torch.ones((128, 1)))
+        dv = state.grad[:, 1:].detach()
+        mu = ((0.5) * (1 / self.r) * self.g * dv)
+        self.v.zero_grad()
+        return mu
+
+    def forward(self, state, action):
+        v = self.v(state)
+        v.backward(torch.ones((128, 1)))
+        dv = state.grad[:, 1:].detach()
+        mu = ((0.5) * (1 / self.r) * self.g * dv)
+        action_mu = (action - mu).unsqueeze(2)
+        A = -(0.5) * self.dt * self.r * \
+            torch.bmm(action_mu.transpose(2, 1),
+                      action_mu)[:, :, 0]
+        self.v.zero_grad()
+        return A + self.v(state)
+
+
+class NAF_R_G:
+
+    def __init__(self, v_model,
+                 noise, state_shape, action_shape,
+                 action_max, dt, r, g, batch_size=200, gamma=0.9999, action_min=None, learning_n_per_fit=8):
+        self.learning_n_per_fit = learning_n_per_fit
+        self.state_shape = state_shape
+        self.action_shape = action_shape
+        self.action_max = action_max
+        if action_min:
+            self.action_min = action_min
+        else:
+            self.action_min = -action_max
+        self.Q = Q_model(v_model, action_shape, dt, r, g, action_max)
+        self.opt = torch.optim.Adam(self.Q.parameters(), lr=1e-4)
+        self.loss = nn.MSELoss()
+        self.Q_target = deepcopy(self.Q)
+        self.tau = 1e-3
+        self.memory = deque(maxlen=100000)
+        self.gamma = gamma
+
+        self.batch_size = batch_size
+        self.noise = noise
+
+    def get_action(self, state, with_noise=True):
+        state = torch.tensor(state, dtype=torch.float)
+        mu_value = self.Q.mu(state).detach().data.numpy() * self.action_max
+        noise = self.noise.noise() * with_noise
+        action = mu_value + noise
+        return np.clip(action, - self.action_max, self.action_max)
+
+    def state_dict(self):
+        return self.Q_target.state_dict()
+
+    def update_targets(self, target, original):
+        for target_param, original_param in zip(target.parameters(), original.parameters()):
+            target_param.data.copy_((1 - self.tau) * target_param.data + self.tau * original_param.data)
+
+    def get_batch(self):
+        minibatch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, dones, next_states = map(np.array, zip(*minibatch))
+        states = torch.tensor(states, dtype=torch.float32, requires_grad=True)
+        actions = torch.tensor(actions, dtype=torch.float32)
+        rewards = torch.tensor(rewards, dtype=torch.float32)
+        dones = torch.from_numpy(dones.astype(np.float32))
+        next_states = torch.tensor(next_states, dtype=torch.float32)
+        return states, actions, rewards, dones, next_states
+
+    def fit(self, state, action, reward, done, next_state):
+        self.memory.append([state, action, reward, done, next_state])
+
+        if len(self.memory) >= self.batch_size:
+            for _ in range(self.learning_n_per_fit):
+                states, actions, rewards, dones, next_states = self.get_batch()
+                self.opt.zero_grad()
+                target = rewards.reshape(self.batch_size, 1) + (
+                        1 - dones).reshape(self.batch_size, 1) * self.gamma * self.Q_target.v(
+                    next_states).detach()
+                loss = self.loss(self.Q(states, actions), target)
+                loss.backward()
+                self.opt.step()
+                self.update_targets(self.Q_target, self.Q)
---
+++ solutions/terminal_pendulum/naf_r.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+
+from models.naf import NAFAgent
+from models.naf_r import NAF_R
+from problems.simple_control_problem.simple_control_problem_env import SimpleControlProblem
+from problems.terminal_pendulum.terminal_pendulum_env import TerminalPendulumEnv
+from utilities.noises import OUNoise
+from utilities.sequentialNetwork import Seq_Network
+
+
+def play_and_learn(env):
+    total_reward = 0
+    state = env.reset()
+    done = False
+    step = 0
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done, _ = env.step(action)
+        total_reward += reward
+        agent.fit(state, action, -reward, done, next_state)
+        state = next_state
+        step += 1
+    agent.noise.decrease()
+    return total_reward
+
+
+def agent_play(env, agent):
+    state = env.reset()
+    total_reward = 0
+    ts = []
+    us = []
+    done = False
+    while not done:
+        ts.append(state[0])
+        us.append(state[1])
+        action = agent.get_action(state)
+        next_state, reward, done, _ = env.step(action)
+        total_reward += reward
+        state = next_state
+    plt.plot(ts, us)
+    return total_reward
+
+
+for i in range(10):
+    env = TerminalPendulumEnv()
+    state_shape = 4
+    action_shape = 1
+    action_max = env.max_torque
+    episodes_n = 100
+    epsilon_min = 0.01
+    epsilon = 1
+    batch_size = 128
+
+    mu_model = Seq_Network([state_shape, 256, 128, action_shape], nn.ReLU(), nn.Tanh())
+    v_model = Seq_Network([state_shape, 256, 128, 1], nn.ReLU())
+    noise = OUNoise(action_shape, threshold=epsilon, threshold_min=epsilon_min,
+                    threshold_decrease=(epsilon_min / epsilon) ** (1 / episodes_n))
+    agent = NAF_R(mu_model, v_model, noise, state_shape, action_shape,
+                  action_max=action_max,
+                  r=env.r, dt=env.dt,
+                  batch_size=batch_size, gamma=0.999, learning_n_per_fit=8)
+
+    rewards = np.zeros(episodes_n)
+    mean_rewards = np.zeros(episodes_n)
+    for episode in range(episodes_n):
+        reward = play_and_learn(env)
+        rewards[episode] = reward
+        mean_reward = np.mean(rewards[max(0, episode - 25):episode + 1])
+        mean_rewards[episode] = mean_reward
+        print("episode=%.0f, noise_threshold=%.3f, total reward=%.3f, mean reward=%.3f, iteration=%.1f" % (
+            episode, agent.noise.threshold, rewards[episode], mean_reward, i))
+
+    np.save('./test/naf_test/' + str(i), mean_rewards)
