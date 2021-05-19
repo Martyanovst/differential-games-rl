ec08401038bba562d37a802ad7de572c6b00ec1f
Martyanovst <martyanovst@gmail.com>
2021-05-19 16:03:41 +0500

    dubins car

--- Solutions/VanDerPol/sphere.py
+++ Solutions/VanDerPol/sphere.py
@@ -12,4 +12,4 @@ tester = AgentTestingModule(env)
 path = './../../Tests/VanDerPol/SPHERE/'
 
 agent_generator = AgentGenerator(env, batch_size, episode_n, noise_min)
-tester.test_agent(agent_generator.generate_naf_sphere_case, episode_n, session_len=1000, epoch_n=epoch_n, path=path, dt_array=[1])
+tester.test_agent(agent_generator.generate_naf_sphere_case, episode_n, session_len=1000, epoch_n=epoch_n, path=path, dt_array=[0.5])
