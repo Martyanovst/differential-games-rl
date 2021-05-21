e52a4c31dbefac53ef00a32738dd79fb32ea6704
smartyanov <smartyanov@naumen.ru>
2021-05-21 15:36:34 +0500

    graphics

--- Solutions/VanDerPol/sphere_r.py
+++ Solutions/VanDerPol/sphere_r.py
@@ -12,4 +12,4 @@ tester = AgentTestingModule(env)
 path = './../../Tests/VanDerPol/SPHERE_R_DT/'
 
 agent_generator = AgentGenerator(env, batch_size, episode_n, noise_min)
-tester.test_agent(agent_generator.generate_naf_sphere_case_r_based, episode_n, session_len=1000, epoch_n=epoch_n, path=path, dt_array=[1, 0.5])
+tester.test_agent(agent_generator.generate_naf_sphere_case_r_based, episode_n, session_len=1000, epoch_n=epoch_n, path=path, dt_array=[1, 0.25])
