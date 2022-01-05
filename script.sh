ba80e86fe813dbe6b7f328c859e4a3727897e99d
smartyanov <smartyanov@naumen.ru>
2022-01-05 21:48:32 +0500

    Linear noise decreasing

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
-python train.py --config configs/targetProblem/naf.json
-python train.py --config configs/targetProblem/bnaf.json
-python train.py --config configs/targetProblem/rb-bnaf.json
-python train.py --config configs/targetProblem/gb-bnaf.json
-python train.py --config configs/targetProblem/ddpg.json
+#python3 train.py --config configs/targetProblem/naf.json
+#python3 train.py --config configs/targetProblem/bnaf.json
+#python3 train.py --config configs/targetProblem/rb-bnaf.json
+#python3 train.py --config configs/targetProblem/gb-bnaf.json
+#python3 train.py --config configs/targetProblem/ddpg.json
 
-python train.py --config configs/pendulum/naf.json
-python train.py --config configs/pendulum/bnaf.json
-python train.py --config configs/pendulum/rb-bnaf.json
-python train.py --config configs/pendulum/gb-bnaf.json
-python train.py --config configs/pendulum/ddpg.json
+python3 train.py --config configs/pendulum/naf.json
+python3 train.py --config configs/pendulum/bnaf.json
+python3 train.py --config configs/pendulum/rb-bnaf.json
+python3 train.py --config configs/pendulum/gb-bnaf.json
+python3 train.py --config configs/pendulum/ddpg.json
 
 python3 train.py --config configs/dubinsCar/naf.json
 python3 train.py --config configs/dubinsCar/bnaf.json
